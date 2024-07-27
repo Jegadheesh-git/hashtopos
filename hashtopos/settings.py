824d159e9844350e6293beced122d2895c471466
Jegadheesh <53766606+Jegadheesh-git@users.noreply.github.com>
2024-07-27 11:27:30 +0530

    Update settings.py

--- hashtopos/settings.py
+++ hashtopos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zrpkk#4%1oskjh_4-tfa)*cn#%eo_$5!8*k3*ez6c$(pte=)&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hashtopos-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = [ 'https://hashtopos-production.up.railway.app']
